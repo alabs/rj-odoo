4f367645d30e5cb0cccc0e00afafdcae5f54daf6
zohaibrehman <zohaibrehman1991@gmail.com>
2020-05-06 23:51:02 +0500

    [Update] fields on pop up

--- addons/rj_record_dev/__manifest__.py
+++ addons/rj_record_dev/__manifest__.py
@@ -11,7 +11,7 @@
     'website': 'https://alabs.org',
     'category': 'Project',
     'license': 'AGPL-3',
-    'version': '0.15',
+    'version': '0.16',
     'installable': True,
     'application': True,
 
